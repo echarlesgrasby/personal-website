a74a8262120694a36a2f368cf05870d9978d9345
Eric <eric@Erics-MBP.hsd1.ar.comcast.net>
2020-10-05 19:45:30 -0500

    init content for new page

---
+++ templates/about-me.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<head>
+	<title>Eric - Home</title>
+	<link rel="stylesheet" type="text/css" href="../styles/styles.css"/>
+	<link rel="icon" href="https://echarlesgrasby.xyz/favicon.ico" /> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="utf-8"> 
+</head>
+<body>
+	<?php include('./templates/header.php') ?>
+	<?php include('../pages/about-me-content.html') ?>
+	<?php include('./templates/footer.php') ?>
+</body>
+</html> 
